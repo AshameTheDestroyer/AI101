f4c7ef888eb95c2613d4e83cae7c4a60de60380c
AshameTheDestroyer <hashemwnoos@gmail.com>
2025-02-09 19:38:52 +0300

    Logistic Regression.

---
+++ LogisticRegression.py
@@ -0,0 +1,52 @@
+import csv
+import numpy as np
+from random import randrange
+import matplotlib.pyplot as plt
+
+def PopulateHouseData():
+    with open("student_data.csv", "w", newline="") as file:
+        writer = csv.writer(file)
+        fields = ["hours", "passed"]
+
+        writer.writerow(fields)
+        minimum, maximum = 4, 60
+        for i in range(minimum, maximum + 1):
+            hours = i
+            passed = 1 if randrange(1, 10) > 8 else 1 if i > 30 else 0
+            writer.writerow([hours, passed])
+
+def GetStudentData():
+    with open("student_data.csv", "r", newline="") as file:
+        reader = csv.reader(file)
+        reader.__next__()
+        return [[int(hours), int(passed)] for [hours, passed] in reader]
+
+def PlotModel(*, args: dict, W: list[float], alpha: float):
+    if (args.populate):
+        PopulateHouseData()
+
+    data = GetStudentData()
+    n, cost = len(data), None
+    X = np.array([x - np.median([x_ for [x_, _] in data]) for [x, _] in data])
+    Y = np.array([y for [_, y] in data])
+
+    for _ in range(10000):
+        Y_ = 1 / (1 + np.exp(-sum([W[i] * X ** i for i in range(len(W))])))
+        cost = sum([-Y[i] * np.log(Y_[i]) - (1 - Y[i]) * np.log(1 - Y_[i]) for i in range(n)]) / n
+        print(cost, W)
+        W_ = []
+        for wi in range(len(W)):
+            dw = sum([-(Y[i] - Y_[i]) * X[i] ** wi for i in range(n)]) / n
+            W_.append(alpha * dw)
+        W = [W[i] - W_[i] for i in range(len(W))]
+
+    X_ = np.linspace(min(X), max(X))
+    Y_ = 1 / (1 + np.exp(-sum([W[i] * X_ ** i for i in range(len(W))])))
+
+    plt.scatter(X, Y)
+    plt.xlabel("Hours")
+    plt.ylabel("Passed")
+    plt.title("Student Passing According to Study Hours")
+    plt.grid(True)
+    plt.plot(X_, Y_)
+    plt.show()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import argparse
-from LinearRegression import PlotModel
+from LogisticRegression import PlotModel
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--populate", type=bool, default=False)
 args = parser.parse_args()
 
-PlotModel(args=args, W=[0, 1, 1, 1], alpha=0.5)
+PlotModel(args=args, W=[0, 1], alpha=0.001)
